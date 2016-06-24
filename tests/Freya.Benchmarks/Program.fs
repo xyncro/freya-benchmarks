6e2322cd2f352721c39023d0460630a348494dc4
kolektiv <andrew@xyncro.com>
2016-06-24 01:04:18 +0100

    init

---
+++ tests/Freya.Benchmarks/Program.fs
@@ -0,0 +1,47 @@
+﻿// Freya
+
+open Freya.Core
+open Freya.Machines.Http
+open Freya.Routers.UriTemplate
+open Freya.Testing
+open Freya.Types.Http
+open Freya.Types.Language
+
+let ok =
+    freya {
+        return {
+            Data = "Hello World!"B
+            Description =
+                { Charset = Some Charset.Utf8
+                  Encodings = None
+                  MediaType = Some MediaType.Text
+                  Languages = Some [ LanguageTag.parse "en-gb"
+                                     LanguageTag.parse "en" ] } } }
+
+let machine =
+    freyaMachine {
+        handleOk ok }
+
+let router =
+    freyaRouter {
+        route GET "/" machine }
+
+// BenchmarkDotNet
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
+type Benchmarks () =
+
+    [<Benchmark>]
+    member __.Default () =
+        evaluate Freya.empty router
+
+// Main
+
+[<EntryPoint>]
+let main _ =
+
+    let summary = BenchmarkRunner.Run<Benchmarks> ()
+
+    0
